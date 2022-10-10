516c995b8c5139d9221a17bfafb1930ae10c5ce4
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-10 15:20:23 +0530

    Add project files.

---
+++ Employee/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Employee
+{
+   
+ 
+        public class Employee
+        {
+            private int id, salary;
+            private String name;
+
+            public void Add(int id,string name,int salary)
+            {
+                this.id = id;
+                this.name = name;
+                this.salary = salary;
+
+            }
+            public String Print()
+            {
+                return $"Employee:{id}/{name}/{salary}";
+            }
+        }
+    
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.Add(122,"joe",20000);
+            Console.WriteLine(e1.Print());
+
+            Employee e2 = new Employee();
+            e2.Add(123, "john", 25000);
+            Console.WriteLine(e2.Print());
+
+        }
+    }
+}
